d5175c347ad0c61b200d0cbd411088c2a6d481a4
tanimpersonal <tashintanim2019@gmail.com>
2022-05-23 00:32:49 +0600

    .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 app.get("/", (req, res) => {
   res.send("Hello World Mara Kha!");
 });
