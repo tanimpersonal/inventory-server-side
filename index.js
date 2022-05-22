92e1f82289a99cd2009964814f3a4ee344077316
tanimpersonal <tashintanim2019@gmail.com>
2022-05-23 00:56:17 +0600

    product json shown

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
-const bodyParser = require("body-parser");
 app.use(express.json());
-//middleware end
 require("dotenv").config();
+//middleware end
+//MONGO CONNECT
 
+const res = require("express/lib/response");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.755op.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+//MONGO CONNECT END
+//get all products
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("assignment-11").collection("products");
+
+    // query for movies that have a runtime less than 15 minutes
+    app.get("/products", async (req, res) => {
+      const query = {};
+
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run();
 app.get("/", (req, res) => {
   res.send("Hello World Mara Kha!");
 });
