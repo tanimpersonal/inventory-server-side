2a9f4160a4ea991bdd5ef7f8e26d085b82a8d620
tanimpersonal <tashintanim2019@gmail.com>
2022-05-23 00:36:04 +0600

    body parser

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+const bodyParser = require("body-parser");
+app.use(express.json());
+//middleware end
 require("dotenv").config();
+
 app.get("/", (req, res) => {
   res.send("Hello World Mara Kha!");
 });
